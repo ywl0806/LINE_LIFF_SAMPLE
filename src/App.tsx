a28f5936df04ca3a3a03d49f2a35530396821bf7
yongwoo <ywl0806@gmail.com>
2023-03-02 13:58:18 +0900

    env

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react";
 import liff from "@line/liff";
 import "./App.css";
+export interface Profile {
+  userId: string;
+  displayName: string;
+  pictureUrl?: string;
+  statusMessage?: string;
+}
 
 function App() {
-  const [name, setName] = useState("");
+  const [profile, setProfile] = useState<Profile>({
+    userId: "",
+    displayName: "",
+  });
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
   const env = import.meta.env;
@@ -17,14 +26,9 @@ function App() {
 
         if (!liff.isLoggedIn()) liff.login();
 
-        liff.getProfile().then((profile) => {
-          console.log("ログインしてるユーザーのid:" + profile.userId);
-          console.log("ログインしてるユーザーの名前:" + profile.displayName);
-          console.log("ログインしてるユーザーの画像URL:" + profile.pictureUrl);
-        });
+        const newProfile = await liff.getProfile();
 
-        const os = liff.getOS() ?? "";
-        setName(os);
+        setProfile(newProfile);
       })
       .catch((e: Error) => {
         setMessage("LIFF init failed.");
@@ -36,7 +40,9 @@ function App() {
     <div className="App">
       <h1>create-liff-app</h1>
       <h2>{env.VITE_LIFF_ID}</h2>
-      {name && <h1>hello {name}</h1>}
+      <h3>{profile.displayName}</h3>
+      <h3>{profile.statusMessage}</h3>
+      <h3>{profile.userId}</h3>
       {message && <p>{message}</p>}
       {error && (
         <p>
